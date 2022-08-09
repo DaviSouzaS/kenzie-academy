28a8cdfdf3cb42d07a1c1ea833bae3bd1b7b2ef2
davi <daavisouza.santana@gmail.com>
2022-08-09 16:03:21 -0300

    S3-10

---
+++ m1/sprint_3/atividades/s3_10_atividade_javascript_trabalhando_strings/index.js
@@ -0,0 +1,8 @@
+//let precoFormatado = NOME DA VARIAVEL.toLocaleString('pt-br', {style: 'currency', currency: 'BRL'}); MUITO ÚTIL
+
+let cumprimento = 'Bom dia'
+let nome = 'Adalberto'
+let sobrenome = 'Silva'
+
+let mensagemCumprimento = `${cumprimento}, ${nome} ${sobrenome}`
+
