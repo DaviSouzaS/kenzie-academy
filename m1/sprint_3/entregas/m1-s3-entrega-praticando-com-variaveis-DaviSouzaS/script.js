741985e70a622f75e1c44a9f89333f5a79939c41
davi <daavisouza.santana@gmail.com>
2022-08-09 16:38:27 -0300

    S3-13 Entrega

---
+++ m1/sprint_3/entregas/m1-s3-entrega-praticando-com-variaveis-DaviSouzaS/script.js
@@ -0,0 +1,8 @@
+let nome = 'Davi'
+let sobrenome = 'Santana' 
+let anoAtual = 2022 
+let anoNascimento = 2003
+let idade = anoAtual - anoNascimento 
+let cidade = 'Salvador'
+
+console.log(`Eu sou o ${nome} ${sobrenome} tenho ${idade} e moro em ${cidade}`)
