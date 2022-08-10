50f09098616f3d8170f652fb27ea1b3936a0c2c8
davi <daavisouza.santana@gmail.com>
2022-08-10 08:45:30 -0300

    S3-13

---
+++ m1/sprint_3/conteudo/javascript_operadores_logicos/index.js
@@ -0,0 +1,34 @@
+//NOT ! (Negação)
+let a = true
+let result1 = !a 
+console.log(result1)
+//FALSE 
+
+//OR || (Ou)
+
+let c = true
+let d = false
+let result2 = c||d
+console.log(result2) 
+//TRUE 
+
+let c1 = false
+let d1 = false
+let result2a = c1 || d1 
+console.log(result2a)
+//FALSE
+
+//&& AND (E)
+
+let e = true
+let f = false
+let result3 = e && f
+console.log(result3) 
+//FALSE 
+
+let e1 = true
+let f1 = true
+let result3a = e1 && f1
+console.log(result3a)
+//TRUE 
+
