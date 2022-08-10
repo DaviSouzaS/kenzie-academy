49b52e8b950e6f2c89dcfdaec74b37a2f9f8c02d
davi <daavisouza.santana@gmail.com>
2022-08-10 11:52:33 -0300

    S3-14

---
+++ m1/sprint_3/atividades/s3_14_atividade_javascript_operadores_logicos/index.js
@@ -0,0 +1,15 @@
+//Exercício 1 
+let idade = 24
+let avaliarIdade = idade > 18 && idade < 25 
+console.log('A idade está entre 18 e 25')
+
+//Exercício 2 
+let carroNome = 'Chevrolet'
+let carroAno = 2019
+let filtrarCarro = carroNome == 'Chevrolet' && carroAno == 2019
+console.log('Carro encontrado')
+
+//Exercício 3 
+let botaoMenu = false
+let avaliarClick = !botaoMenu
+console.log('fechar menu')
