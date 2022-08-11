d0886a34520a64ca443be1645f28c2210cf44788
davi <daavisouza.santana@gmail.com>
2022-08-11 20:57:10 -0300

    S3-18

---
+++ m1/sprint_3/atividades/s3_18_atividade_lista_exercicios_algoritimos/index.js
@@ -0,0 +1,74 @@
+//Exercício 1 
+
+let n1 = 10 
+alert(`numero inteiro ${n1}`)
+
+//Exercício 2
+
+let numeroint1 = 5
+let numeroint2 = 9
+let numeroint3 = 4 
+
+alert(numeroint1 + numeroint2 + numeroint3)
+
+//Exercíco 3
+
+let nota1 = 7
+let nota2 = 9
+
+alert((nota1+nota2)/2)
+
+//Exercício 4 
+
+let numeroAleatorio = 7 
+
+alert(numeroAleatorio%2)
+
+//Exercício 5 
+
+let numberR = 10 
+
+alert(numberR**2)
+
+//Exercício 6 
+
+let num1 = 4
+let num2 = 8 
+let num3 = 8
+let num4 = 7 
+
+alert((num1*1 + num2*2 + num3*3 +num4*4)/(1 + 2 + 3 + 4))
+
+//Exercício 7 
+
+let mat = 4
+let port = 2 
+let gerais = 6
+let infor = 9 
+
+alert((mat*2+port*1+gerais*1+infor*1)/(1+2+3+4))
+
+//Exercício 8 
+
+let dividendo = 20 
+let divisor = 4
+
+alert(dividendo/divisor) 
+alert(dividendo%divisor)
+
+//Exercício 9 
+
+let saldo = parseInt(prompt('Saldo'))
+alert(saldo + saldo/100)
+
+//Exercício 10 
+
+let diagonalMenor = parseInt(prompt('Diagonal Menor'))
+let diagonalMaior = parseInt(prompt('Diagonal Maior'))
+
+alert((diagonalMaior * diagonalMenor)/2)
+
+
+
+
+//Acabei deixando as coisas meio desorganizadas, mas lendo a atividade com calma e olhando exercício por exercicio é possível entender perfeitamente. 
