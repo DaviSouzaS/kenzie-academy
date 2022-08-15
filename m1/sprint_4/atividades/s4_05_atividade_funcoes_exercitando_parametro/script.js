1b361a0d3dea5209bb1aad4712983a7d1243faf0
davi <daavisouza.santana@gmail.com>
2022-08-15 15:56:04 -0300

    S4-05 incompleto

---
+++ m1/sprint_4/atividades/s4_05_atividade_funcoes_exercitando_parametro/script.js
@@ -0,0 +1,30 @@
+//Exercício 1
+
+function soma (valor1, valor2) {
+    return valor1 + valor2
+}
+
+console.log (soma (15,876)) //891
+
+//Exercício 2
+
+function identificadorValor (tipo) {
+    return typeof(tipo)
+}
+
+console.log (identificadorValor (10))
+
+console.log ('O valor (' + 10 + ') é do tipo (' + typeof(10) + ')')
+
+//Exercício 3 
+
+function somarNotas (nota1, nota2, nota3, nota4) {
+    
+    return nota1 + nota2 + nota3 + nota4
+}
+
+console.log(somarNotas(8, 6, 9, 3))
+
+//Ainda não consegui fazer com que a função só aceite numbers 
+
+
