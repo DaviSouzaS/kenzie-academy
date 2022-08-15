4364194f65695e45d5dc9d41920695386477dc62
davi <daavisouza.santana@gmail.com>
2022-08-15 14:50:19 -0300

    S4-04

---
+++ m1/sprint_4/atividades/s4_04_atividade_funcoes_passo_a_passo/script.js
@@ -0,0 +1,11 @@
+function textReturn (text) {
+    return text
+}
+
+console.log(textReturn('Olá, estou sendo retornado pela função'))
+
+function textTexto (text2) {
+    console.log(text2) 
+}
+
+textTexto('Olá, este é o text 2')
