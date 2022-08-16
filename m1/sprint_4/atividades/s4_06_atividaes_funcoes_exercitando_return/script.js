c7b1e340c32a22ff4cb34d403ce5747c84cd978a
davi <daavisouza.santana@gmail.com>
2022-08-16 12:31:04 -0300

    S4-06 iniciado

---
+++ m1/sprint_4/atividades/s4_06_atividaes_funcoes_exercitando_return/script.js
@@ -0,0 +1,17 @@
+//Exercício 1
+
+function somarNotas (nota1, nota2, nota3, nota4) {
+
+    return nota1 + nota2 + nota3 + nota4
+
+}
+
+function media (somarNotas) {
+   return somarNotas/4 
+}
+
+console.log(somarNotas(8,7,8,7))
+console.log(`Média do aluno é: ${media(somarNotas(8,7,8,7))}`)
+
+//Exercício 2
+
