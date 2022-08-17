0dd797cd6b1a937f0162fadb687764387559c961
davi <daavisouza.santana@gmail.com>
2022-08-17 11:44:52 -0300

    S4-10

---
+++ m1/sprint_4/atividades/s4_10_atividade_array_exercitando_acesso_dados/script.js
@@ -0,0 +1,19 @@
+//Exercício 1 
+let nomeDosCompetidores = ["Maria", "Heric", "Roberta", "Rafael", "Lucas"]
+
+console.log(nomeDosCompetidores[0])
+
+//Exercíco 2
+console.log(nomeDosCompetidores[4])
+
+//Exercício 3 
+let posicoesDoPodio = [1,2,3,4,5]
+
+//Maria em 1º
+console.log(nomeDosCompetidores[0], posicoesDoPodio[0])
+
+//Heric em 2º
+console.log(nomeDosCompetidores[1], posicoesDoPodio[1])
+
+//Roberta em 3º
+console.log(nomeDosCompetidores[2], posicoesDoPodio[2])
