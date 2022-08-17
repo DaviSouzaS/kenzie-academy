97932beac248057d206ee7e26de728b8cb273b82
davi <daavisouza.santana@gmail.com>
2022-08-17 11:20:26 -0300

    s4-09 completo

--- m1/sprint_4/atividades/s4_09_atividade_arrays_passo_a_passo/script.js
+++ m1/sprint_4/atividades/s4_09_atividade_arrays_passo_a_passo/script.js
@@ -1,5 +1,13 @@
 let minhaPrimeiraLista = ["Matemática","Química","Geografia", "Português","Física",]
 
-minhaPrimeiraLista[2]= 'História'
+minhaPrimeiraLista[2]= 'História' //modifiquei geografia para historia 
+
+minhaPrimeiraLista.push("Biologia") //Adicionei biologia no final do array 
+
+minhaPrimeiraLista.pop() //Remove o último item, no caso Biologia
+
+minhaPrimeiraLista.unshift('Literatura') // Adiciona um item no começo do array, no caso Literatura 
+
+minhaPrimeiraLista.shift() //Remove o primeiro item do array, no caso literatura
 
 console.log(minhaPrimeiraLista)
