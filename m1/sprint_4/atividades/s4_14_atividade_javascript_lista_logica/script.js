a4af9dea76d1c4c82cbd3c8ad9d9fa4c572ac532
davi <daavisouza.santana@gmail.com>
2022-08-21 11:39:27 -0300

    S4-14 travado

--- m1/sprint_4/atividades/s4_14_atividade_javascript_lista_logica/script.js
+++ m1/sprint_4/atividades/s4_14_atividade_javascript_lista_logica/script.js
@@ -9,7 +9,7 @@ console.log(calculaIdade(2003))
 //Atividade 2 
 function calculaDiferencaAno (anoInicial, anoFinal) {
     let periodo = anoInicial - anoFinal
-    if (periodo <0){
+    if (periodo < 0){
         periodo = periodo * (-1)
     }
     return periodo
@@ -45,3 +45,16 @@ return salarioCompleto
 console.log(calculaValorSalario(1200,1800))
 
 //Atividade 5 
+
+function notaDaPosicao (listaNotas, listaPosição) {
+
+let notas = [5, 7, 10, 3, 9, 5, 5, 2, 1, 9, 8, 3, 4, 6, 7, 6, 7, 8, 10, 7] 
+
+for (let i = 0; i <= notas.length; i++) {
+    notas = notas[i] 
+}
+
+//Não Faço ideia de como fazer a lista de notas ser selecionada pelo parâmetro listaNotas, e a listaPosição selecionar o [i]
+}
+
+
