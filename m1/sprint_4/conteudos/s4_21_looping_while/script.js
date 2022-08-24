8fe2c8663db48ef6e8a5607e7f965ff44bc7987e
davi <daavisouza.santana@gmail.com>
2022-08-24 07:42:57 -0300

    S4-21

---
+++ m1/sprint_4/conteudos/s4_21_looping_while/script.js
@@ -0,0 +1,16 @@
+//Tipo de loop bem símples
+
+let n = 1 //Valor de inicialização
+
+while (n < 3)  { //Condição 'n < 3'
+
+    console.log(n)// Código a ser 'rodado'
+
+    n += 1 //Código a ser 'rodado' (Poderia usar somente 'n++' ao invés de 'n += 1')
+}
+
+console.log('O loop acabou.') //Código a ser rodado quando o loop acabar
+
+//O loop diz: Enquanto 'n' for menor que 3, o console.log vai mostrar o 'n' no console, e 1 será incrementado no n. Quando o loop acabar, ou seja 'n' parar de ser menor que 3, outro console.log mostra no console 'O loop acabou'
+
+//O 'n' é inicializado com 1, o loop checa para ver se a condição é verdadeira, e ela é. então é incrementado 1 no 'n' e isso continua até a condição se tornar falsa. 
