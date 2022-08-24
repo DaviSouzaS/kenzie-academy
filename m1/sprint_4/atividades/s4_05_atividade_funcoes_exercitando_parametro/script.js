e00dbbfaec1779bd4216ca9891e4949c59f3ed57
davi <daavisouza.santana@gmail.com>
2022-08-24 09:25:48 -0300

    S4-05

--- m1/sprint_4/atividades/s4_05_atividade_funcoes_exercitando_parametro/script.js
+++ m1/sprint_4/atividades/s4_05_atividade_funcoes_exercitando_parametro/script.js
@@ -6,6 +6,8 @@ function soma (valor1, valor2) {
 
 console.log (soma (15,876)) //891
 
+
+
 //Exercício 2
 
 function identificadorValor (tipo) {
@@ -16,15 +18,23 @@ console.log (identificadorValor (10))
 
 console.log ('O valor (' + 10 + ') é do tipo (' + typeof(10) + ')')
 
+
+
 //Exercício 3 
 
 function somarNotas (nota1, nota2, nota3, nota4) {
+    let numero = 1
     
-    return nota1 + nota2 + nota3 + nota4
+    if (typeof(nota1) != typeof(numero) || typeof(nota2) != typeof(numero) || typeof(nota3) != typeof(numero) || typeof(nota4) != typeof(numero)) {
+        console.log('O campo só pode receber números')
+    } 
+    else {
+            return nota1 + nota2 + nota3 + nota4
+    }
+
 }
 
 console.log(somarNotas(8, 6, 9, 3))
 
-//Ainda não consegui fazer com que a função só aceite numbers 
 
 
