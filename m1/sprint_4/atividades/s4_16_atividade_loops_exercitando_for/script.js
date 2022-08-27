c6a0f596b6fb33209323594ad117b3cf9c2e02d7
davi <daavisouza.santana@gmail.com>
2022-08-27 13:35:54 -0300

    s4-16

---
+++ m1/sprint_4/atividades/s4_16_atividade_loops_exercitando_for/script.js
@@ -0,0 +1,38 @@
+//🧠 Exercício 1 - Mostrando números pares de um array
+
+function numerosPares (listaNumerica) {
+
+    let str = ''
+    let arrayPares = []
+    for (let i = 0; i <= listaNumerica.length - 1 ; i++) {
+        str = listaNumerica[i]
+
+        if (str % 2 == 0) {
+            arrayPares.push(str)
+        }
+    }
+    return arrayPares
+}
+const arr = [5, 4, 7, 30, 16, 89] 
+console.log(numerosPares(arr))
+
+//🧠 Exercício 2 - Somando números de um array
+
+function somaNumeros (listaNumbers) {
+
+    let soma = 0
+    for (let i = 0; i <= listaNumbers.length - 1; i++) {
+        
+        
+        if (typeof(listaNumbers[i]) == typeof('a')) {
+            //transformar '4' em number
+        }
+        soma += listaNumbers[i] 
+    }
+
+    return soma
+}
+
+
+let arrayNumeros = [1, '4', 9, 18]
+console.log(somaNumeros(arrayNumeros))
