b0e85daec108253c3197c014c60e3d1c0bfd0cb9
davi <daavisouza.santana@gmail.com>
2022-09-04 09:16:29 -0300

    S7-03

---
+++ m1/sprint_7/atividades/s7_03_atividade_praticando_conhecimentos_sobre_DOM/script.js
@@ -0,0 +1,54 @@
+//Kata 1
+
+// const container = document.getElementById('root')
+// console.log(container)
+
+
+
+//Kata 2 
+
+// const container = document.getElementById('root')
+// container.innerText = 'repetir'
+
+
+
+//Kata 3 
+
+// const principal = document.getElementById('root')
+// principal.innerText = 'repetir mais'
+
+
+
+//Kata 4 
+
+// const repetir = document.createElement('span')
+
+
+
+//Kata 5 
+
+// const mod = document.createElement('span')
+// mod.innerText = 'preciso repetir'
+
+
+
+//Kata 6 
+
+// const denovo = document.createElement('span')
+// const raiz = document.getElementById('root')
+// denovo.innerText = 'repetir e repetir'
+// raiz.append(denovo)
+
+
+
+//Kata 7
+
+// const lista = document.createElement('ul')
+// const item = document.createElement('li')
+
+// item.innerText = 'repetir cada vez mais'
+// lista.appendChild(item)
+
+// const origem = document.getElementById('root')
+
+// origem.appendChild(lista)
