0ce4a8736f1f62b9c8682b5e117aabceeca0563f
davi <daavisouza.santana@gmail.com>
2022-10-17 16:28:20 -0300

    S5-01

---
+++ m2/sprint_5/conteudos/s5_01_EsModules/index.js
@@ -0,0 +1,18 @@
+import { frutas, carros } from "/product.js"
+
+frutas.map((fruta) => {
+    console.log(fruta);
+});
+
+carros.map((carro) => {
+    console.log(carro)
+})
+
+
+
+
+
+import { sum } from "./sum.js"
+
+console.log(sum(1, 2))
+
---
+++ m2/sprint_5/conteudos/s5_01_EsModules/product.js
@@ -0,0 +1,39 @@
+export const frutas = [
+    {
+      id: 1,
+      nome: "banana",
+      preco: 4,
+    },
+    {
+      id: 2,
+      nome: "laranja",
+      preco: 6,
+    },
+    {
+      id: 3,
+      nome: "abacate",
+      preco: 5,
+    },
+  ];
+
+// export { frutas }
+
+export const carros = [
+    {
+      id: 1,
+      nome: "Carro 1",
+      ano: 2015,
+    },
+    {
+      id: 2,
+      nome: "Carro 2",
+      ano: 2019,
+    },
+  ];
+
+// export { carros }
+
+
+
+
+  
